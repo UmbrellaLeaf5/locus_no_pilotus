3c9ac49ebc0501f4178e8343813626c497500988
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-02 04:35:04 +0300

    start to coding

--- lib/base.h
+++ lib/base.h
@@ -1,1 +1,15 @@
 #pragma once
+
+typedef void* json;
+
+namespace LIB {
+
+class JSONable {
+ public:
+  // JSONable(const json& j);
+
+  virtual json save() const = 0;
+  virtual JSONable load(const json& j) = 0;
+};
+
+}  // namespace LIB
