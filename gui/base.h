7f6a6376d9b1dab97f1f149529270282ede0c8bf
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-07 19:58:48 +0300

    update gui: add gui::Target and update ui

--- gui/base.h
+++ gui/base.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#pragma once
+
+#include "../lib/target.h"
+
+namespace gui {
+class Drawable {
+ public:
+  virtual void Draw() const = 0;
+};
+}  // namespace gui
--- gui/target.h
+++ gui/target.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "../lib/target.h"
+#include "base.h"
+
+namespace gui {
+class Target : public Drawable {
+ public:
+  void Draw() const override;
+
+  inline void SetX(double x) { data.SetX(x); }
+  inline void SetY(double y) { data.SetY(y); }
+
+  inline double GetX() const { return data.GetX(); }
+  inline double GetY() const { return data.GetY(); }
+
+ private:
+  lib::Target data;
+};
+}  // namespace gui
