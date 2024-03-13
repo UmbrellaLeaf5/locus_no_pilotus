1be5c67f897c18c19f2ec5da8e9a9369e918a732
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-13 19:23:12 +0300

    add default constructor lib::Target

--- lib/target.h
+++ lib/target.h
@@ -6,6 +6,8 @@ namespace lib {
 class Target : public JSONable {
  public:
   inline Target(double x, double y) : x_{x}, y_{y} {}
+  inline Target() : x_{0}, y_{0} {}
+
   json Save() const override;
   JSONable* Load(const json& j) override;
 
