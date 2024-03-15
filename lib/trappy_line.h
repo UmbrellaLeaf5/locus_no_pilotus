b0c4e7fbe71c3976a14d8b9cff4dd9e68a5e787d
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-16 00:38:06 +0300

    create new constructors for lib::TrappyLine (with initializer_list)

--- lib/trappy_line.h
+++ lib/trappy_line.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <initializer_list>
 #include <vector>
 
 #include "base.h"
@@ -15,13 +16,26 @@ class TrappyLine : public JSONable {
   // конструктор происходит исключительно по КТ,
   // так как только между ними может быть перелет
 
+  inline TrappyLine(std::initializer_list<lib::Target> targets)
+      : targets_{targets} {}
+
   inline TrappyLine(std::vector<lib::Target> targets) : targets_{targets} {}
 
   json Save() const override;
   JSONable* Load(const json& j) override;
 
+  inline void SetNewTargets(std::initializer_list<lib::Target> targets) {
+    targets_ = targets;
+  }
+
   inline void SetNewTargets(std::vector<lib::Target> targets) {
     targets_ = targets;
+  }
+
+  inline void AddTargets(std::initializer_list<lib::Target> targets) {
+    for (const auto& target : targets) {
+      targets_.push_back(target);
+    }
   }
 
   inline void AddTargets(std::vector<lib::Target> targets) {
