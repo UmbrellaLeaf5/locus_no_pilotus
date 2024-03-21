21f7009b753499ea1447e45b3dc237b6bbe173b6
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-22 00:55:40 +0300

    add icon and change window title

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
+  w.setWindowTitle("Trajectory Calculation");
+  w.setWindowIcon(QIcon("../images/icon.png"));
+
   w.show();
   return a.exec();
 }
