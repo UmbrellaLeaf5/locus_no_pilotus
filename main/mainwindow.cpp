fb04b022f5ac2a06a8f21c6c8f552fd59fdef9dd
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-26 00:03:41 +0300

    try git-hook with commenting extra lib lines

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include "../gui/target.h"
 #include "../gui/trappy_circle.h"
 #include "../gui/trappy_line.h"
+// #include "../icecream-cpp/icecream.hpp"  // IGNORE
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent)
@@ -12,10 +13,13 @@ MainWindow::MainWindow(QWidget* parent)
   manager_.SetPlot(ui->plot);
 
   {  // проверка функционала графических классов и менеджера
-
+//     IC();  // IGNORE
     gui::Target t_1(3, 4);
     gui::Target t_2(2, 1);
     gui::Target t_3(1, 1);
+
+//     IC(t_3.GetPoint().x);  // IGNORE
+//     IC(t_3.GetPoint().y);  // IGNORE
 
     manager_.Set({t_1, t_2, t_3});
 
