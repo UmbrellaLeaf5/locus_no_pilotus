705af17882c5493fa5898c907b6f5b99afd03251
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-26 21:14:10 +0300

    add CMake function add_icecream_cpp_lib

---
+++ cmake_scripts/scripts_for_icecream_cpp.cmake
@@ -0,0 +1,16 @@
+function(add_icecream_cpp_lib START_DIR)
+  if(EXISTS "${START_DIR}/../icecream-cpp")
+    list(APPEND CMAKE_PREFIX_PATH "${START_DIR}/../icecream-cpp")
+    find_package(IcecreamCpp)
+    include_directories(SYSTEM ${IcecreamCpp_INCLUDE_DIRS})
+    return()
+  endif()
+
+  file(GLOB ABOVEFOLDERS LIST_DIRECTORIES true "${START_DIR}/../*")
+
+  foreach(ABOVEFOLDER ${ABOVEFOLDERS})
+    if(IS_DIRECTORY ${ABOVEFOLDER})
+      add_icecream_cpp_lib(${ABOVEFOLDER})
+    endif()
+  endforeach()
+endfunction()
