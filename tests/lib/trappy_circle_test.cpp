b6d57ddc540e3d4f48ea744660e31e1c021f184b
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-29 00:05:05 +0300

    start test implementation all lib classes and delete useless .h file

---
+++ tests/lib/trappy_circle_test.cpp
@@ -0,0 +1,19 @@
+#include "../lib/trappy_circle.h"
+
+#include <QApplication>
+#include <random>
+
+// #include "icecream.hpp"  //IGNORE
+
+// #include "qcustomplot.h"
+
+#if !defined(WIN32)
+#define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+namespace tt = boost::test_tools;
+namespace utf = boost::unit_test;
+using namespace lib;
+
+BOOST_AUTO_TEST_SUITE(lib_trappy_circle)
