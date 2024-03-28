206caab833d8b15d903879bf1a205d2c108b676b
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-29 01:57:44 +0300

    add complete testing lib::TrappyCircle and delete useless TrappyLine testing

--- tests/lib/trappy_circle_test.cpp
+++ tests/lib/trappy_circle_test.cpp
@@ -17,5 +17,67 @@ namespace utf = boost::unit_test;
 using namespace lib;
 
 BOOST_AUTO_TEST_SUITE(lib_trappy_circle)
-//
+BOOST_AUTO_TEST_CASE(simple_construct) {
+  struct {
+    double x, y, r;
+  } cases[] = {{0, 0, 0}, {0, 1, 0}, {1, 0, 1}};
+
+  // конструктор по (double, double)
+  for (auto c : cases) {
+    TrappyCircle t_c({c.x, c.y}, c.r);
+
+    BOOST_TEST(t_c.GetCenter().x == c.x);
+    BOOST_TEST(t_c.GetCenter().y == c.y);
+    BOOST_TEST(t_c.GetRadius() == c.r);
+  }
+
+  // конструктор по lib::Point
+  for (auto c : cases) {
+    TrappyCircle t_c({c.x, c.y}, c.r);
+
+    BOOST_TEST(t_c.GetCenter().x == c.x);
+    BOOST_TEST(t_c.GetCenter().y == c.y);
+    BOOST_TEST(t_c.GetRadius() == c.r);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(random_get) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-1000, 1000);
+  std::uniform_real_distribution<> dis_pos(0, 1000);
+
+  for (int i = 0; i < 1000; i++) {
+    double x = dis(gen), y = dis(gen), r = dis_pos(gen);
+
+    TrappyCircle t_c({x, y}, r);
+
+    BOOST_TEST(t_c.GetCenter().x == x);
+    BOOST_TEST(t_c.GetCenter().y == y);
+    BOOST_TEST(t_c.GetRadius() == r);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(random_set) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-1000, 1000);
+  std::uniform_real_distribution<> dis_pos(0, 1000);
+
+  TrappyCircle t_c({0, 0}, 0);
+  for (int i = 0; i < 1000; i++) {
+    double x = dis(gen), y = dis(gen), r = dis_pos(gen);
+
+    t_c.SetCenter({x, y});
+    t_c.SetRadius(r);
+
+    BOOST_TEST(t_c.GetCenter().x == x);
+    BOOST_TEST(t_c.GetCenter().y == y);
+    BOOST_TEST(t_c.GetRadius() == r);
+  }
+}
+
+// BOOST_AUTO_TEST_CASE(json_load, *utf::disabled()) {}
+// BOOST_AUTO_TEST_CASE(json_save, *utf::disabled()) {}
+
 BOOST_AUTO_TEST_SUITE_END()
