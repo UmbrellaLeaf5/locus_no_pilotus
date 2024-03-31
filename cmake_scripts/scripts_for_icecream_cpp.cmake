358e110a565d1336889faf2c28a70934092b802c
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-31 19:13:34 +0300

    include icecream-cpp to main subdirectory

---
+++ cmake_scripts/scripts_for_icecream_cpp.cmake
@@ -0,0 +1,12 @@
+function(move_icecream_hpp_to_build_dir START_DIR)
+  if(EXISTS "${START_DIR}/icecream-cpp")
+    file(GLOB ICLUDEHEADERFILES "${START_DIR}/icecream-cpp/*.hpp" "${START_DIR}/icecream-cpp/*.h")
+
+    foreach(file ${ICLUDEHEADERFILES})
+      file(COPY ${file} DESTINATION "${CMAKE_BINARY_DIR}/include")
+    endforeach()
+    return()
+  else()
+    message(FATAL_ERROR "${START_DIR}/icecream-cpp does not exist, please install submodule 'https://github.com/renatoGarcia/icecream-cpp'")
+  endif()
+endfunction()
