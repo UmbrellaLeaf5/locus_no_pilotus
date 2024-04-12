f0dcf21a49215f239a9e595f700190ee0a054550
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-04-12 14:42:27 +0300

    change adding id to objects in json file

--- main/gui_json_file/save.cpp
+++ main/gui_json_file/save.cpp
@@ -17,36 +17,36 @@ void GuiJsonFile::Save(PlotArea& plot_area) {
   QJsonArray trappy_lines_array;
   QJsonArray hills_array;
 
-  int id = 10001;
+  int add_to_id = 10000;
 
   for (size_t i = 0; i < plot_area.GetTargets().size(); i++) {
+    int id = add_to_id + plot_area.GetTargets()[i].GetPlottableIndex();
     targets_array.append(plot_area.GetTargets()[i].GetData().Save(id));
-    id++;
   }
   root.insert("Targets", targets_array);
 
-  id = 20001;
+  add_to_id = 20000;
 
   for (size_t i = 0; i < plot_area.GetTrappyCircles().size(); i++) {
+    int id = add_to_id + plot_area.GetTrappyCircles()[i].GetItemIndex();
     trappy_circles_array.append(
         plot_area.GetTrappyCircles()[i].GetData().Save(id));
-    id++;
   }
   root.insert("Trappy_Circles", trappy_circles_array);
 
-  id = 30001;
+  add_to_id = 30000;
 
   for (size_t i = 0; i < plot_area.GetTrappyLines().size(); i++) {
+    int id = add_to_id + plot_area.GetTrappyLines()[i].GetPlottableIndex();
     trappy_lines_array.append(plot_area.GetTrappyLines()[i].GetData().Save(id));
-    id++;
   }
   root.insert("Trappy_Lines", trappy_lines_array);
 
-  id = 40001;
+  add_to_id = 40000;
 
   for (size_t i = 0; i < plot_area.GetHills().size(); i++) {
+    int id = add_to_id + plot_area.GetHills()[i].GetPlottableIndex();
     hills_array.append(plot_area.GetHills()[i].GetData().Save(id));
-    id++;
   }
   root.insert("Hills", hills_array);
 
