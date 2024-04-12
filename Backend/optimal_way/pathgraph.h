4d267605d25caa0505c4ebb1672dfe6cd1768986
Romanov-Fedor <romanov.fn@phystech.edu>
2024-04-12 22:51:26 +0300

    Update structure

--- Backend/optimal_way/pathgraph.h
+++ Backend/optimal_way/pathgraph.h
@@ -4,23 +4,27 @@
 #include <vector>
 
 #include "circleobstacle.h"
+
 namespace math {
+
+struct Path;
 
 struct PathWayNode {
   PathWayNode(Point p) : point{p} {}
-  std::vector<std::shared_ptr<PathWayNode>> edges;
+  std::vector<Path> edges;
   std::shared_ptr<CircleObstacle> circle_prt = nullptr;
   Point point;
 };
 
-class PathGraph {
- public:
-  PathGraph(std::vector<CircleObstacle> obstacles, Point point1, Point point2);
-
- private:
-  std::vector<PathWayNode> graph_;
-
-  double FindTheShortestWay();
+struct Path {
+  Path(std::shared_ptr<PathWayNode> node_ptr, double length)
+      : next{node_ptr}, length{length} {}
+  std::shared_ptr<PathWayNode> next;
+  double length;
 };
 
+void ConnectTwoNodes(PathWayNode& node1, PathWayNode& node2, double distance) {
+  node1.edges.push_back({std::make_shared<PathWayNode>(node2), distance});
+  node2.edges.push_back({std::make_shared<PathWayNode>(node1), distance});
+}
 }  // namespace math
