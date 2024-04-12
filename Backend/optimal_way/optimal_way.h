999d2f78b3227e9332cb71085e3ded76465f8f64
Romanov-Fedor <romanov.fn@phystech.edu>
2024-04-12 16:08:14 +0300

    Remove function from class

--- Backend/optimal_way/optimal_way.h
+++ Backend/optimal_way/optimal_way.h
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-#include "pathgraph.h"
+#include "circleobstacle.h"
 
 namespace math {
 
@@ -18,25 +18,6 @@ class MinimumDistanceCalculator {
   Point point2_;
   std::vector<CircleObstacle> circles_;
   std::vector<double> ways_;
-
-  double DistanceBetweenPoints(const Point& p1, const Point p2) const;
-
-  LinearАunction MakeLinearАunction(const Point& point1,
-                                    const Point& point2) const;
-  // Точки касания прямой к окружснотям
-  std::pair<Point, Point> CrossPoints(const LinearАunction& tangent,
-                                      const CircleObstacle& circle1,
-                                      const CircleObstacle& circle2) const;
-
-  std::pair<Point, Point> TangentPointsToCircle(const CircleObstacle& crcl,
-                                                const Point& point) const;
-
-  std::vector<LinearАunction> TangentsBetweenCircles(
-      const CircleObstacle& circle1, const CircleObstacle& circle2) const;
-
-  // Пересекает ли отрезок, проведенный через две точки, окружность
-  bool AreThereIntersections(const CircleObstacle& crcl, const Point& pnt1,
-                             const Point& pnt2) const;
 
   // Заполняет вектор точек касательных для каждой окружности
   void FillTangentsCircles();
