c65781d6afe13d3cd96e7e80d6709be8d866295b
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-04-12 11:38:25 +0300

    Algorithm beautify

--- Backend/dijkstras_algorithm/points_graph.cpp
+++ Backend/dijkstras_algorithm/points_graph.cpp
@@ -5,11 +5,11 @@
 void Dijkstras_algorithm::Calculate_Min_Paths() {
   while (graphs_vertex_[end_] > min_length_) {
     std::shared_ptr<PointNode> min_len_key;
-    for (auto&& elem : graphs_vertex_)
+    for (auto& elem : graphs_vertex_)
       if ((elem.second == min_length_) && ((*elem.first).is_useful))
         min_len_key = elem.first;
-    min_length_ = inf;
-    for (auto& elem : (*min_len_key).edges)
+
+    for (auto& elem : (*min_len_key).paths)
       if ((graphs_vertex_.find(elem.end_point) == graphs_vertex_.end()) ||
           (elem.length + graphs_vertex_[min_len_key] <
            graphs_vertex_[elem.end_point])) {
@@ -20,11 +20,11 @@ void Dijkstras_algorithm::Calculate_Min_Paths() {
         (*elem.end_point).is_useful = false;
         continue;
       }
-    min_length_ = inf;
     (*min_len_key).is_useful = false;
-    for (auto& elem : graphs_vertex_) {
+
+    min_length_ = inf;
+    for (auto& elem : graphs_vertex_)
       if ((elem.second < min_length_) && ((*elem.first).is_useful))
         min_length_ = elem.second;
-    }
   }
 }
