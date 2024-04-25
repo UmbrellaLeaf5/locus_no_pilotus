715d4fe4351a640bba8026413ebb522a8aef2b65
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-25 22:52:06 +0300

    add comments for lib::Point

--- lib/point.h
+++ lib/point.h
@@ -3,8 +3,9 @@
 #include <cmath>
 
 namespace lib {
-constexpr double precision = 0.000001;
+constexpr double precision = 1.0E-6;
 
+/// @brief Математическая точка
 struct Point {
   double x;
   double y;
@@ -31,9 +32,18 @@ struct Point {
 inline Point operator+(Point a, Point b) { return a += b; }
 inline Point operator-(Point a, Point b) { return a -= b; }
 
+/**
+ * @brief Оператор сравнения двух точек
+ * @details Работает как примерно равно (с точностью precision = 1.0E-6)
+ * @param a: первая точка
+ * @param b: вторая точка
+ * @return true: если точки примерно равны
+ * @return false: если точки не равны даже примерно
+ */
 inline bool operator==(Point a, Point b) {
   return std::abs(a.x - b.x) < precision && std::abs(a.y - b.y) < precision;
 }
+
 inline bool operator!=(Point a, Point b) { return !(a == b); }
 
 }  // namespace lib
