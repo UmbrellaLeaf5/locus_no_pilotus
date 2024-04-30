a7b4fcd1c03c0c2d672590d71f0384041b9e6a42
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-30 16:28:45 +0300

    delete useless methods in gui and lib Hill

--- lib/hill.cpp
+++ lib/hill.cpp
@@ -59,44 +59,6 @@ bool Hill::IsChanged(const QJsonObject& hill_obj) const {
   return false;
 }
 
-Hill::Hill(Point center, double radius, std::size_t vertices_amount) {
-  if (vertices_amount == 0 || vertices_amount == 1)
-    throw std::invalid_argument("Hill cannot consist of " +
-                                std::to_string(vertices_amount) + " point!");
-
-  if (radius < 0)
-    throw std::invalid_argument("Hill cannot have of negative radius!");
-
-  for (std::size_t i = 0; i < vertices_amount; i++) {
-    // каждый раз двигаем точку по окружности
-    // делаем это, рассчитывая угол в зависимости от кол-ва вершин
-
-    double angle = 2.0 * M_PI * i / vertices_amount;
-    double x = center.x + radius * cos(angle);
-    double y = center.y + radius * sin(angle);
-
-    vertices_.push_back({x, y});
-  }
-
-  CheckErrorValues();
-}
-
-Point Hill::GetCenter() const {
-  // центр = сумма точек/кол-во точек
-  Point res(0, 0);
-
-  for (const auto& vert : vertices_) res += vert;
-
-  return {res.x / vertices_.size(), res.y / vertices_.size()};
-}
-
-double Hill::GetRadius() const {
-  // радиус = расстояние от центра до любой вершины (т.к. центр равноудален)
-
-  return sqrt(pow((vertices_[0].x - GetCenter().x), 2) +
-              pow((vertices_[0].y - GetCenter().y), 2));
-}
-
 bool Hill::operator==(const Hill& hill) const {
   if (vertices_.size() != hill.GetPoints().size()) return false;
 
