9ba17138ffdbc081ebb711e69b06d6f3cb9aec61
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-05-07 16:27:30 +0300

    struct lib::Segment definition

--- lib/segment.h
+++ lib/segment.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "point.h"
 
 namespace lib {
@@ -8,6 +10,18 @@ namespace lib {
  * @brief Математический отрезок траектории
  * @details Может быть как отрезком, так и дугой некоторой окружности
  */
-struct Segment {};
+class Segment {
+ public:
+  Segment(Point start, Point end) : start{start}, end{end} {}
+
+  Segment(Point start, Point end, Point center)
+      : start{start}, end{end}, center_ptr{&center} {}
+
+  Point start;
+
+  Point end;
+
+  std::unique_ptr<Point> center_ptr = std::nullptr;
+};
 
 }  // namespace lib
