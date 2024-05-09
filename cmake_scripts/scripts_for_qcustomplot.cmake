6b7cfa28f89e2b55591ffb6f4fb2d83f603bcbcc
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-05-09 17:57:26 +0300

    make using cmake_script for copying libqcustomplot.dll

--- cmake_scripts/scripts_for_qcustomplot.cmake
+++ cmake_scripts/scripts_for_qcustomplot.cmake
@@ -2,7 +2,7 @@
 
 function(replace_include_qcustomplot_in_autogen BUILD_DIR)
   if(EXISTS "${BUILD_DIR}/main/main_autogen/include/ui_mainwindow.h")
-    message(STATUS "Found ${BUILD_DIR}/main/main_autogen/include/ui_mainwindow.h, replacing qcustomplot include")
+    message(STATUS "Found ${BUILD_DIR}/main/main_autogen/include/ui_mainwindow.h , replacing qcustomplot include")
 
     # open the file where you need to replace the line in FILE_CONTENT
     file(READ "${BUILD_DIR}/main/main_autogen/include/ui_mainwindow.h" FILE_CONTENT)
@@ -15,5 +15,35 @@ function(replace_include_qcustomplot_in_autogen BUILD_DIR)
     file(WRITE "${BUILD_DIR}/main/main_autogen/include/ui_mainwindow.h" "${MODIFIED_CONTENT}")
   else()
     message(STATUS "warning: ${BUILD_DIR}/main/main_autogen/include does not exist, please build and reconfigure the project")
+  endif()
+endfunction()
+
+function(load_qcustomplot_dll START_DIR)
+  if(EXISTS "${START_DIR}/qcustomplot")
+    # add needed files to QCUSTOMPLOTDLLFILES
+    file(GLOB_RECURSE QCUSTOMPLOTDLLFILES "${CMAKE_BINARY_DIR}/libqcustomplot.dll")
+
+    if(QCUSTOMPLOTDLLFILES)
+      message(STATUS "Found libqcustomplot.dll, copying to build")
+    else()
+      file(GLOB_RECURSE QCUSTOMPLOTDLLFILES "${START_DIR}/libqcustomplot.dll")
+
+      if(QCUSTOMPLOTDLLFILES)
+        message(STATUS "Found ${QCUSTOMPLOTDLLFILES} , copying libqcustomplot.dll to build")
+      else()
+        message(STATUS "warning: libqcustomplot.dll not found in build folder, please build and reconfigure the project")
+      endif()
+    endif()
+
+    # copy from QCUSTOMPLOTDLLFILES to dir in binary
+    foreach(file ${QCUSTOMPLOTDLLFILES})
+      if(QCUSTOMPLOTDLLFILES)
+        # copy to main, where qcustomplot is used
+        file(COPY ${file} DESTINATION ${CMAKE_BINARY_DIR}/main)
+      endif()
+    endforeach()
+    return()
+  else()
+    message(STATUS "warning: ${START_DIR}/qcustomplot does not exist, please install submodule 'https://github.com/legerch/QCustomPlot-library'")
   endif()
 endfunction()
