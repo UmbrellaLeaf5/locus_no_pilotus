2985d55fbb891e5e80f5b5519051a629a0b2cc6f
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-05-09 20:29:48 +0300

    move images to .extra

--- cmake_paths.cmake
+++ cmake_paths.cmake
@@ -3,7 +3,7 @@
 cmake_path(SET QCUSTOMPLOT_DIR "${CMAKE_SOURCE_DIR}/qcustomplot")
 cmake_path(SET ICECREAM-CPP_DIR "${CMAKE_SOURCE_DIR}/icecream-cpp")
 cmake_path(SET CMAKESCRIPTS_DIR "${CMAKE_SOURCE_DIR}/cmake_scripts")
-cmake_path(SET IMAGES_DIR "${CMAKE_SOURCE_DIR}/images")
+cmake_path(SET IMAGES_DIR "${CMAKE_SOURCE_DIR}/.extra/images")
 
 cmake_path(SET PROJECT_DIR "${CMAKE_SOURCE_DIR}")
 
