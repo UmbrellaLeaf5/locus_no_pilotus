d0453a9c079e91089daa17981bef0dc1ce4036b5
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-05-10 04:27:35 +0300

    circle -> png picture

--- gui/flying_robot.cpp
+++ gui/flying_robot.cpp
@@ -74,7 +74,9 @@ void gui::FlyingRobot::Draw(QCustomPlot* plot) {
 
   graph_->setPen(QColor(50, 50, 50, 255));
   graph_->setLineStyle(QCPGraph::lsNone);
-  graph_->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 4));
+  graph_->setScatterStyle(
+      QCPScatterStyle(QPixmap("../images/flying_robot.png")
+                          .scaled(QSize(24, 24), Qt::KeepAspectRatio)));
 
   graph_->setData({curr_point_.x}, {curr_point_.y});
 }
