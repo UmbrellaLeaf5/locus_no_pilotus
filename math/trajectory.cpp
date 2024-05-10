b79ec3cf1cf081027e05a3553629b4d78cb407e7
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-05-10 03:59:23 +0300

    start TrajectoryCalculator class

--- math/trajectory.cpp
+++ math/trajectory.cpp
@@ -3,6 +3,33 @@
 
 namespace math {
 
-TrajectoryCalculator::TrajectoryCalculator() {}
+std::vector<Segment> TrajectoryCalculator::GetTrajectoryPart();
+
+void TrajectoryCalculator::CalculateTrajectory() {
+  std::vector<std::vector<double>> matrix(targets_.size());
+  for (auto& elem : matrix) elem.resize(targets_.size());
+
+  std::vector<std::vector<std::vector<Segment>>> optimal_ways(targets_.size());
+  for (auto& elem : optimal_ways) elem.resize(targets_.size());
+
+  OptimalWayCalculator owc(circles_, polys_);
+
+  for (std::size_t i = 0; i < targets_.size() - 1; ++i) {
+    matrix[i][i] = inf;
+    for (std::size_t j = i + 1; i < targets_.size(); ++j) {
+      std::vector<std::size_t> optimal_way =
+          owc.GetOptimalWay(targets_[i], targets_[j]);
+      PathWayGraph graph = owc.GetGraph();
+
+      std::vector<Segment> segment_way = GetTrajectoryPart(optimal_way, graph);
+      optimal_ways[i][j] = segment_way;
+      std::reverse(segment_way.begin(), segment_way.begin());
+      optimal_ways[j][i] = segment_way;
+
+      matrix[i][j] = owc.GetOptimalWayLength();
+      matrix[j][i] = owc.GetOptimalWayLength();
+    }
+  }
+}
 
 }  // namespace math
--- math/trajectory.h
+++ math/trajectory.h
@@ -6,6 +6,7 @@
 #include "math/optimal_way/optimal_way.h"
 
 using lib::Hill;
+using lib::Segment;
 using lib::Target;
 using lib::TrappyCircle;
 using lib::TrappyLine;
@@ -14,7 +15,63 @@ namespace math {
 
 class TrajectoryCalculator {
  public:
-  TrajectoryCalculator();
+  /**
+   * @brief Инициализирует экземпляр класса TrajectoryCalculator
+   * @param targets: контрольные точки
+   * @param lines: запрещённые перелеты
+   * @param circles: круговые препятствия
+   * @param hills: многоугольные препятствия
+   */
+  TrajectoryCalculator(const std::vector<Target>& targets,
+                       const std::vector<TrappyLine>& lines,
+                       const std::vector<TrappyCircle>& circles,
+                       const std::vector<Hill>& hills) {
+    for (auto& target : targets) targets_.push_back(Point(target.GetPoint()));
+
+    forbidden_lines_ = lines;
+
+    for (auto& circle : circles)
+      circles_.push_back(
+          CircleObstacle(Point(circle.GetCenter()), circle.GetRadius()));
+
+    for (auto& hill : hills) {
+      std::vector<Point> vertexes;
+      for (auto& vertex : hill.GetVertices()) vertexes.push_back(Point(vertex));
+      polys_.push_back(PolygonObstacle(vertexes));
+    }
+
+    CalculateTrajectory();
+  }
+
+  std::vector<Segment> GetTrajectory() { return trajectory_; }
+
+ private:
+  // Контрольные точки
+  std::vector<Point> targets_;
+
+  // Запрещенные перелёты
+  std::vector<TrappyLine> forbidden_lines_;
+
+  // Круговые препятствия
+  std::vector<CircleObstacle> circles_;
+
+  // Многоугольные препятствия
+  std::vector<PolygonObstacle> polys_;
+
+  // Траектория облета контрольных точек
+  std::vector<Segment> trajectory_;
+
+  /**
+   * @brief Подсчет части траектории
+   * @param optimal_way: порядок обхода точек
+   * @param graph: граф, содеражщий информацию о координатах точек
+   * @return Часть траектории
+   */
+  std::vector<Segment> GetTrajectoryPart(std::vector<std::size_t> optimal_way,
+                                         PathWayGraph graph);
+
+  /// @brief Расcчитывает траекторию
+  void CalculateTrajectory();
 };
 
 }  // namespace math
